2dfa101a22d0587450440ec83b59e8a1a423e3f0
Joao Gardin <gardin1992@gmail.com>
2019-10-18 02:07:47 -0300

    Adicionados actions get Edit e post Edit em FabricantesController

--- Capitulo01/Controllers/FabricantesController.cs
+++ Capitulo01/Controllers/FabricantesController.cs
@@ -3,8 +3,10 @@ using Capitulo01.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace Capitulo01.Controllers
 {
@@ -35,6 +37,40 @@ namespace Capitulo01.Controllers
             context.SaveChanges();
 
             return RedirectToAction("Index");
+        }
+
+        // GET: Edit
+        public ActionResult Edit(long? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Fabricante fabricante = context.Fabricantes.Find(id);
+
+            if (fabricante == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(fabricante);
+        }
+
+        // POST: Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Fabricante fabricante)
+        {
+            if (ModelState.IsValid)
+            {
+                context.Entry(fabricante).State = EntityState.Modified;
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(fabricante);
         }
     }
 }
