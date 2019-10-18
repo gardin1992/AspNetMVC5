616d81dcd5600b776530ba2fd10da452a7900de2
Joao Gardin <gardin1992@gmail.com>
2019-10-18 01:34:13 -0300

    Add controller fabricantes

---
+++ Capitulo01/Contexts/EFContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Capitulo01.Models;
+
+namespace Capitulo01.Contexts
+{
+    public class EFContext : DbContext
+    {
+        public EFContext() : base("Asp_Net_MVC_CS") { }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Fabricante> Fabricantes { get; set; }
+    }
+}
---
+++ Capitulo01/Controllers/FabricantesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Capitulo01.Controllers
+{
+    public class FabricantesController : Controller
+    {
+        // GET: Fabricantes
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
