4abadc47f14b277e888a75f0ed572d6fc8824dd2
Joao Gardin <gardin1992@gmail.com>
2019-10-18 02:00:43 -0300

    Adicionados metodos Create e View do FabricantesController

--- Capitulo01/Controllers/FabricantesController.cs
+++ Capitulo01/Controllers/FabricantesController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Capitulo01.Contexts;
+using Capitulo01.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,31 @@ namespace Capitulo01.Controllers
 {
     public class FabricantesController : Controller
     {
+        private EFContext context = new EFContext();
+
         // GET: Fabricantes
         public ActionResult Index()
         {
+            return View(context.Fabricantes.OrderBy(c => c.Nome));
+        }
+
+        // GET: Create
+        public ActionResult Create()
+        {
+
             return View();
+        }
+
+        // POST: Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Fabricante fabricante)
+        {
+
+            context.Fabricantes.Add(fabricante);
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
         }
     }
 }
