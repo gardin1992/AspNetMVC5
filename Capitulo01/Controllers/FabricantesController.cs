d1103d38f591aa83acfa4835eec61adf0074da9f
Joao Gardin <gardin1992@gmail.com>
2019-10-22 11:47:01 -0300

    Add partial views em details Fabricantes

--- Capitulo01/Controllers/FabricantesController.cs
+++ Capitulo01/Controllers/FabricantesController.cs
@@ -81,7 +81,7 @@ namespace Capitulo01.Controllers
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
 
-            Fabricante fabricante = context.Fabricantes.Find(id);
+            Fabricante fabricante = context.Fabricantes.Where(f => f.FabricanteId == id).Include("Produtos.Categoria").First();
 
             if (fabricante == null)
             {
