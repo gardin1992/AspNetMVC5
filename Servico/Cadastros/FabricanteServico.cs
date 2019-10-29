ff15b889c17643f65e6b58070d243ff28222b5b2
Joao Gardin <gardin1992@gmail.com>
2019-10-29 05:16:20 -0300

    Adicionado o componente de Servico

---
+++ Servico/Cadastros/FabricanteServico.cs
@@ -0,0 +1,20 @@
+﻿using Modelo.Cadastros;
+using Persistencia.DAL.Cadastros;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servico.Cadastros
+{
+    public class FabricanteServico
+    {
+        private FabricanteDAL fabricanteDAL = new FabricanteDAL();
+
+        public IQueryable<Fabricante> ObterFabricantesClassificadosPorNome()
+        {
+            return fabricanteDAL.ObterFabricantesClassificadosPorNome();
+        }
+    }
+}
---
+++ Servico/Tabelas/CategoriaServico.cs
@@ -0,0 +1,20 @@
+﻿using Modelo.Tabelas;
+using Persistencia.DAL.Tabelas;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servico.Tabelas
+{
+    public class CategoriaServico
+    {
+        private CategoriaDAL categoriaDAL = new CategoriaDAL();
+
+        public IQueryable<Categoria> ObterCategoriasClassificadasPorNome()
+        {
+            return categoriaDAL.ObterCategoriasClassificadosPorNome();
+        }
+    }
+}
