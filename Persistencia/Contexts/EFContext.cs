7d1e7a6372626aad162bb00128920b09c3191f65
Joao Gardin <gardin1992@gmail.com>
2019-10-29 11:15:38 -0300

    EntityFramework 6.2, Atualizado AspNetMvc e configurando o Migration

--- Persistencia/Contexts/EFContext.cs
+++ Persistencia/Contexts/EFContext.cs
@@ -6,6 +6,7 @@ using System.Data.Entity;
 using Modelo.Cadastros;
 using Modelo.Tabelas;
 using System.Data.Entity.ModelConfiguration.Conventions;
+using Persistencia.Migrations;
 
 namespace Persistencia.Contexts
 {
@@ -14,7 +15,7 @@ namespace Persistencia.Contexts
         public EFContext() : base("Asp_Net_MVC_CS") 
         {
             Database.SetInitializer<EFContext>(
-                new DropCreateDatabaseIfModelChanges<EFContext>());
+                new MigrateDatabaseToLatestVersion<EFContext, Configuration>());
         }
         public DbSet<Categoria> Categorias { get; set; }
         public DbSet<Fabricante> Fabricantes { get; set; }
