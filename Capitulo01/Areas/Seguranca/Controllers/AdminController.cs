52e21f7e52438437938a519ffd8fcdbafa0ac4dd
Joao Gardin <gardin1992@gmail.com>
2019-10-31 01:10:32 -0300

    Adicionado metodos e views delete e details para AdminController, crud finalizado

--- Capitulo01/Areas/Seguranca/Controllers/AdminController.cs
+++ Capitulo01/Areas/Seguranca/Controllers/AdminController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.Owin;
 using Capitulo01.Infraestrutura;
 using Capitulo01.Areas.Seguranca.Models;
+using System.Net;
 
 namespace Capitulo01.Areas.Seguranca.Controllers
 {
@@ -47,6 +48,82 @@ namespace Capitulo01.Areas.Seguranca.Controllers
             return View(model);
         }
 
+        public ActionResult Edit(string id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuario usuario = GerenciadorUsuario.FindById(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            var uvm = new UsuarioViewModel();
+            uvm.Id = usuario.Id;
+            uvm.Nome = usuario.UserName;
+            uvm.Email = usuario.Email;
+            return View(uvm);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(UsuarioViewModel uvm)
+        {
+            if (ModelState.IsValid)
+            {
+                Usuario usuario = GerenciadorUsuario.FindById(uvm.Id);
+                usuario.UserName = uvm.Nome;
+                usuario.Email = uvm.Email;
+                usuario.PasswordHash = GerenciadorUsuario.PasswordHasher.HashPassword(uvm.Senha);
+                IdentityResult result = GerenciadorUsuario.Update(usuario);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    AddErrosFromResult(result);
+                }
+            }
+            return View(uvm);
+        }
+
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuario usuario = GerenciadorUsuario.FindById(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        [HttpPost]
+        public ActionResult Delete(Usuario usuario)
+        {
+            Usuario user = GerenciadorUsuario.FindById(usuario.Id);
+            if (user != null)
+            {
+                IdentityResult result = GerenciadorUsuario.Delete(user);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+            }
+            else
+            {
+                return HttpNotFound();
+            }
+        }
         private void AddErrosFromResult(IdentityResult result)
         {
             foreach(string error in result.Errors)
